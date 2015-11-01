f1476904ec9b764999a2f72256fc902c393ebdc9
Sebastian Holager <bassetassen@hotmail.com>
2015-11-01 22:36:30 +0100

    added handling of number attributes on create

--- test/requestFactoryTests.js
+++ test/requestFactoryTests.js
@@ -27,5 +27,21 @@
 			expect(keys[1].firstChild.data).to.equal('abc');
 			expect(values[1].firstChild.data).to.equal('aaa');
 		});
+
+		it('uses correct datatypes', function() {
+			var result = requests.create('', {}, 'contact', [{logicalName: 'name', value: 'test'}, {logicalName:'age', value:12, type:'int'}]);
+
+			var xmlBody = new Dom().parseFromString(result);
+			var keys = xpath.select('//*[local-name() = \'key\']', xmlBody);
+			var values = xpath.select('//*[local-name() = \'value\']', xmlBody);
+
+			expect(keys[0].firstChild.data).to.equal('name');
+			expect(values[0].firstChild.data).to.equal('test');
+			expect(values[0].attributes[0].value).to.equal('c:string');
+
+			expect(keys[1].firstChild.data).to.equal('age');
+			expect(values[1].firstChild.data).to.equal('12');
+			expect(values[1].attributes[0].value).to.equal('c:int');
+		});
 	});
 })();
